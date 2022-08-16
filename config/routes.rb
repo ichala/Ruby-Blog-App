80688a577a999c3eacb9b1f038c8fe1133b194a5
ichala <alijandhd@gmail.com>
2022-08-16 01:05:28 +0100

    Update Layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get '/users', to: "users#index"
   get '/users/:id/', to: "users#user"
   get '/users/:id/posts', to: "users#posts"
+  get '/new', to: "users#new_post"
+  post '/new', to: "users#save_post"
+  post '/addcomment/:id', to: "users#addcomment"
+  get 'addlike/:id', to: "users#addlike"
   get '/users/:id/posts/:id', to: "users#post"
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
