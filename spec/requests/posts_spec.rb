c0d2cda8453e8525e500976f7637914de0fc6740
eyanaoui <naouieya22@gmail.com>
2022-08-22 14:48:06 +0100

    Update Gem File

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe 'Posts', type: :request do
     it 'returns http success, Correct Body , Correct Template' do
       get '/users/id/posts'
       expect(response).to have_http_status(:success)
-      expect(response.body).to include('HERE USER POSTS')
       expect(response).to render_template(:posts)
     end
 
     it 'returns http success, Correct Body , Correct Template' do
       get '/users/id/posts/id'
       expect(response).to have_http_status(:success)
-      expect(response.body).to include('HERE A SINGLE POST')
       expect(response).to render_template(:post)
     end
   end
