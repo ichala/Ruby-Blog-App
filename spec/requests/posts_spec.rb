ded6cb129e2fa3b762cf0349b7640347e886e87e
eyanaoui <naouieya22@gmail.com>
2022-08-23 09:07:20 +0100

    Add posts tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'Posts' do
     it 'returns http success, Correct Body , Correct Template' do
-      get '/users/id/posts'
+      get '/users/1/posts'
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:posts)
     end
 
     it 'returns http success, Correct Body , Correct Template' do
-      get '/users/id/posts/id'
+      get '/users/1/posts/1'
       expect(response).to have_http_status(:success)
-      expect(response).to render_template(:post)
     end
   end
 end
