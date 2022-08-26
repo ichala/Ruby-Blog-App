1edfe86c91e1b1820e91f08e5cc849016ff72700
Aimal Amiri <meaimal@hotmail.com>
2022-08-26 16:17:36 +0430

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  
+
   namespace :api do
     namespace :v1 do
     #Create an API endpoint to list all posts for a user.
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
     #Create an API endpoint to list all comments for a user's post.
     get '/posts/:post_id/comments', to: 'posts#allcoments'
     #Create an API endpoint to add a comment to a post by the current user
+    post '/comment/:post_id/add', to: 'loginrequired#addcomment'
+    #Add authentication to my API endpoints.
+    #Login:
+    post '/auth/login', to: 'authentication#login'
+    #Signup:
+    post '/auth/signup', to: 'users#create'
     end
   end
   get '/*a', to: 'application#not_found'
